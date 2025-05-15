138848698a02c3a28f5f7f811546311f9758b2d3
elikese <elikese@gmail.com>
2025-05-15 09:19:33 +0900

    feat: day12_02

--- day12/day12_02.py
+++ day12/day12_02.py
@@ -52,3 +52,73 @@ test_car(gas_car)
 
 # payment를 함수의 조합으로 구현 -> 함수가 함수를 매개변수로 받아서 사용하는 특징을 사용
 
+
+def order(payment, *menu_names):
+    menu = {
+        "피자": 25000,
+        "치킨": 20000,
+    }
+    price = 0
+    for menu_name in menu_names:
+        price += menu[menu_name]
+
+    final_price = payment(price)
+    print(f"총 결제금액: {final_price}")
+
+
+def kakao_payment(price):
+    # 항상 10프로 할인
+    return price * 0.9
+
+
+def naver_payment(price):
+    # 주문금액 20000원 이상이면 2000원 할인
+    if price >= 20000:
+        return price - 2000
+
+    return price
+
+
+# 함수에 함수를 전달해서, 구체적인 기능구현을 분리
+order(naver_payment, "피자", "치킨")
+
+
+class Payment:
+    def __init__(self):
+        print("결제 시스템 초기화")
+
+    def pay(self, price):
+        pass
+
+
+class KakaoPayment(Payment):
+    def pay(self, price):
+        return price * 0.9  # 10% 할인
+
+
+class NaverPayment(Payment):
+    def pay(self, price):
+        if price >= 20000:
+            return price - 2000  # 2,000원 할인
+        return price
+
+
+def order(payment_instance, *menu_names):
+    menu = {
+        "피자": 25000,
+        "치킨": 20000,
+    }
+    price = 0
+    for menu_name in menu_names:
+        price += menu[menu_name]
+
+    final_price = payment_instance.pay(price)
+    print(f"총 결제금액: {final_price}")
+
+
+kakao = KakaoPayment()
+naver = NaverPayment()
+order(kakao, "피자", "치킨")
+order(naver, "피자", "치킨")
+
+# 함수로 구현하고, 클래스로도 구현이 가능.
